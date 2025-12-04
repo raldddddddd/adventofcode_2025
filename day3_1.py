f70d9c4f1c267d221f1b771f1689a72a9f1c7b4e
raldddddddd <rissantiago31@gmail.com>
2025-12-04 16:03:00 +0800

    Day 3 added

---
+++ day3_1.py
@@ -0,0 +1,23 @@
+total = 0
+length = 2
+while True:
+    bank = input()
+    if bank == "-1":
+        break
+    num = ['0'] * length
+    index = 0
+    pos = 0
+    last = 0
+    while index < length:
+        for i, char in enumerate(bank[pos:len(bank)-(length-index)+1]):
+            if int(char) > int(num[index]):
+                num[index] = char
+                last = i
+        if last == 0:
+            pos += 1
+        else:
+            pos += last + 1
+        index +=1
+    total += int("".join(map(str, num)))
+    
+print(total)
